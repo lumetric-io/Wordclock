b81604f335869536965a4da3220d0a062dcfe5b9
lumetric-io <your-email@domain>
2026-01-17 21:52:24 +0100

    wordclock-logo integration

--- src/display_init.h
+++ src/display_init.h
@@ -3,12 +3,18 @@
 #include "led_state.h"
 #include "display_settings.h"
 #include "log.h"
+#if defined(PRODUCT_VARIANT_LOGO)
+#include "logo_leds.h"
+#endif
 
 // Initialize LED and display settings
 // This function initializes the LED state and display settings from persistent storage.
 // Ensures the clock starts up with correct color and brightness.
 inline void initDisplay() {
     ledState.begin();
+#if defined(PRODUCT_VARIANT_LOGO)
+    logoLeds.begin();
+#endif
     displaySettings.begin();
     const GridVariantInfo* info = getGridVariantInfo(displaySettings.getGridVariant());
     if (info) {
