a416f96d416b7f7a0f4e0133ad5b9a7b05e435b8
lumetric-io <your-email@domain>
2026-01-20 13:16:58 +0100

    Bump wordclock-mini version to mini-0.1.1-dev.22

--- products/wordclock-mini/product_config.h
+++ products/wordclock-mini/product_config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PRODUCT_ID "wordclock-mini"
-#define FIRMWARE_VERSION "mini-0.1.1-dev.21"
-#define UI_VERSION "ui-mini-0.1.1-dev.21"
+#define FIRMWARE_VERSION "mini-0.1.1-dev.22"
+#define UI_VERSION "ui-mini-0.1.1-dev.22"
 #define PRODUCT_VARIANT_MINI 1
 #define DATA_PIN 7
 #define SUPPORT_MINUTE_LEDS 0
