8a163029fdb25ad900d0b896b40c2a96647f6c7b
lumetric-io <your-email@domain>
2026-01-30 11:28:12 +0100

    Bump wordclock-nextgen version to nextgen-0.1.1-dev.5

--- products/wordclock-nextgen/product_config.h
+++ products/wordclock-nextgen/product_config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PRODUCT_ID "wordclock-nextgen"
-#define FIRMWARE_VERSION "nextgen-0.1.1-dev.4"
-#define UI_VERSION "ui-nextgen-0.1.1-dev.4"
+#define FIRMWARE_VERSION "nextgen-0.1.1-dev.5"
+#define UI_VERSION "ui-nextgen-0.1.1-dev.5"
 #define PRODUCT_VARIANT_NEXTGEN 1
 #define DATA_PIN 4
 #define SUPPORT_OTA_V2 1
