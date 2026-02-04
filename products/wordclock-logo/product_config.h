a762e9553194068b62c2373fc0c7f226e2d0acd8
lumetric-io <your-email@domain>
2026-02-04 19:39:03 +0100

    Bump wordclock-logo version to logo-26.2.10-rc.2

--- products/wordclock-logo/product_config.h
+++ products/wordclock-logo/product_config.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #define PRODUCT_ID "wordclock-logo"
-#define FIRMWARE_VERSION "logo-26.2.10-rc.1"
-#define UI_VERSION "ui-logo-26.2.10-rc.1"
+#define FIRMWARE_VERSION "logo-26.2.10-rc.2"
+#define UI_VERSION "ui-logo-26.2.10-rc.2"
 #define PRODUCT_VARIANT_LOGO 1
 #define DATA_PIN 4
+#define LOGO_DATA_PIN 18
 #define SUPPORT_OTA_V2 1
 #define SETUP_BLINK_ENABLED 0
 #define SETUP_BLINK_LED_COUNT 0
