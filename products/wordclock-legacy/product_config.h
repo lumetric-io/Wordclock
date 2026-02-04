1f05f462eb3e713bfcb63b151cece8b3128e70da
lumetric-io <your-email@domain>
2026-02-04 19:50:42 +0100

    Bump wordclock-legacy version to legacy-26.2.6-dev.1

--- products/wordclock-legacy/product_config.h
+++ products/wordclock-legacy/product_config.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #define PRODUCT_ID "wordclock-legacy"
-#define FIRMWARE_VERSION "legacy-26.2.5"
-#define UI_VERSION "ui-legacy-26.2.5"
+#define FIRMWARE_VERSION "legacy-26.2.6-dev.1"
+#define UI_VERSION "ui-legacy-26.2.6-dev.1"
 #define PRODUCT_VARIANT_LEGACY 1
+#define SETUP_ASSUME_DONE_IF_LEGACY_CONFIG 1
 #define DATA_PIN 4
 #define SUPPORT_OTA_V2 0
 #define SETUP_BLINK_ENABLED 0
