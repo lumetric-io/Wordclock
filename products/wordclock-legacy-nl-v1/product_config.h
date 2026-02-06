6fef7287111ec746e101d78884b8212fef6427f6
lumetric-io <your-email@domain>
2026-02-06 20:15:54 +0100

    OTA fixes

--- products/wordclock-legacy-nl-v1/product_config.h
+++ products/wordclock-legacy-nl-v1/product_config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PRODUCT_ID "wordclock-legacy-nl-v1"
-#define FIRMWARE_VERSION "legacy-26.2.6-rc.9"
-#define UI_VERSION "ui-legacy-26.2.6-rc.9"
+#define FIRMWARE_VERSION "legacy-nl-v1-26.2.6-rc.9"
+#define UI_VERSION "ui-legacy-nl-v1-26.2.6-rc.9"
 #define PRODUCT_VARIANT_LEGACY 1
 #define SETUP_ASSUME_DONE_IF_LEGACY_CONFIG 1
 #define DATA_PIN 4
