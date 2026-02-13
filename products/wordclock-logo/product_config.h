fedf3314014e303b6cc24cbe413c6cbce3120267
lumetric-io <your-email@domain>
2026-02-13 12:40:30 +0100

    Bump wordclock-logo version to logo-26.2.11

--- products/wordclock-logo/product_config.h
+++ products/wordclock-logo/product_config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PRODUCT_ID "wordclock-logo"
-#define FIRMWARE_VERSION "logo-26.2.10"
-#define UI_VERSION "ui-logo-26.2.10"
+#define FIRMWARE_VERSION "logo-26.2.11"
+#define UI_VERSION "ui-logo-26.2.11"
 #define PRODUCT_VARIANT_LOGO 1
 #define DATA_PIN 4
 #define LOGO_DATA_PIN 18
