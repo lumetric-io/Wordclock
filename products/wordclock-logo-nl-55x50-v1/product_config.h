4c7f72153155c72d28b4c38db7f075d8ab05df64
lumetric-io <your-email@domain>
2026-02-13 12:14:45 +0100

    Bump wordclock-logo-nl-55x50-v1 version to logo-nl-55x50-v1-26.2.10-rc.10

--- products/wordclock-logo-nl-55x50-v1/product_config.h
+++ products/wordclock-logo-nl-55x50-v1/product_config.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #define PRODUCT_ID "wordclock-logo-nl-55x50-v1"
-#define FIRMWARE_VERSION "logo-nl-55x50-v1-26.2.10-rc.9"
-#define UI_VERSION "ui-logo-nl-55x50-v1-26.2.10-rc.9"
+#define FIRMWARE_VERSION "logo-nl-55x50-v1-26.2.10-rc.10"
+#define UI_VERSION "ui-logo-nl-55x50-v1-26.2.10-rc.10"
 #define PRODUCT_VARIANT_LOGO 1
 #define DATA_PIN 4
-#define LOGO_DATA_PIN 18
+//#define LOGO_DATA_PIN 18
 #define SUPPORT_OTA_V2 1
 #define LED_STATUS_EVENTS_ENABLED 1
 
