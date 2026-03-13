f8ac17c55d62fb78dd10dbd1b7382203639019b6
lumetric-io <your-email@domain>
2026-03-13 13:55:28 +0100

    Bump wordclock-legacy-nl-v4 version to legacy-nl-v4-26.2.11-rc.4

--- products/wordclock-legacy-nl-v4/product_config.h
+++ products/wordclock-legacy-nl-v4/product_config.h
@@ -1,13 +1,13 @@
 #pragma once
 
 #define PRODUCT_ID "wordclock-legacy-nl-v4"
-#define FIRMWARE_VERSION "legacy-nl-v4-26.2.11-rc.3"
-#define UI_VERSION "ui-legacy-nl-v4-26.2.11-rc.3"
+#define FIRMWARE_VERSION "legacy-nl-v4-26.2.11-rc.4"
+#define UI_VERSION "ui-legacy-nl-v4-26.2.11-rc.4"
 #define PRODUCT_VARIANT_LEGACY 1
 #define SETUP_ASSUME_DONE_IF_LEGACY_CONFIG 1
 #define DATA_PIN 4
 #define SUPPORT_OTA_V2 1
-#define LED_STATUS_EVENTS_ENABLED 0
+#define LED_STATUS_EVENTS_ENABLED 1
 
 // Log level: LOG_LEVEL_DEBUG, LOG_LEVEL_INFO, LOG_LEVEL_WARN, LOG_LEVEL_ERROR
 // #define DEFAULT_LOG_LEVEL LOG_LEVEL_ERROR
