0238a02c163ed53ca2ca44946210271a6a4bfdc8
lumetric-io <your-email@domain>
2026-03-20 09:52:41 +0100

    mini grid update

--- src/grid_variants/nl_20x20_v1.cpp
+++ src/grid_variants/nl_20x20_v1.cpp
@@ -1,7 +1,7 @@
 #include "grid_variants/nl_20x20_v1.h"
 
 // Dutch 20x20 V1 layout - no HET/IS words
-const uint16_t LED_COUNT_GRID_NL_20x20_V1 = 106;
+const uint16_t LED_COUNT_GRID_NL_20x20_V1 = 105;
 const uint16_t LED_COUNT_EXTRA_NL_20x20_V1 = 0;
 const uint16_t LED_COUNT_TOTAL_NL_20x20_V1 = LED_COUNT_GRID_NL_20x20_V1 + LED_COUNT_EXTRA_NL_20x20_V1;
 
@@ -18,25 +18,25 @@ const char* const LETTER_GRID_NL_20x20_V1[] = {
 };
 
 const WordPosition WORDS_NL_20x20_V1[] = {
-  { "VIJF_M",      { 21, 20, 19, 18 } },
-  { "TIEN_M",      { 1, 2, 3, 4 } },
-  { "OVER",        { 25, 26, 27, 28 } },
-  { "VOOR",        { 16, 15, 14, 13 } },
-  { "KWART",       { 5, 6, 7, 8, 9 } },
-  { "HALF",        { 30, 31, 32, 33 } },
-  { "UUR",         { 103, 104, 105 } },
-  { "EEN",         { 49, 50, 51 } },
-  { "TWEE",        { 40, 39, 38, 37 } },
-  { "DRIE",        { 53, 54, 55, 56 } },
-  { "VIER",        { 69, 68, 67, 66 } },
-  { "VIJF",        { 65, 64, 63, 62 } },
-  { "ZES",         { 57, 61, 81 } },
-  { "ZEVEN",       { 45, 44, 43, 42, 41 } },
-  { "ACHT",        { 73, 74, 75, 76 } },
-  { "NEGEN",       { 97, 98, 99, 100, 101 } },
-  { "TIEN",        { 77, 78, 79, 80 } },
-  { "ELF",         { 87, 86, 85 } },
-  { "TWAALF",      { 93, 92, 91, 90, 89, 88 } }
+  { "VIJF_M",      { 20, 19, 18, 17 } },
+  { "TIEN_M",      { 0, 1, 2, 3 } },
+  { "OVER",        { 24, 25, 26, 27 } },
+  { "VOOR",        { 15, 14, 13, 12 } },
+  { "KWART",       { 4, 5, 6, 7, 8 } },
+  { "HALF",        { 29, 30, 31, 32 } },
+  { "UUR",         { 102, 103, 104 } },
+  { "EEN",         { 48, 49, 50 } },
+  { "TWEE",        { 39, 38, 37, 36 } },
+  { "DRIE",        { 52, 53, 54, 55 } },
+  { "VIER",        { 68, 67, 66, 65 } },
+  { "VIJF",        { 64, 63, 62, 61 } },
+  { "ZES",         { 56, 60, 80 } },
+  { "ZEVEN",       { 44, 43, 42, 41, 40 } },
+  { "ACHT",        { 72, 73, 74, 75 } },
+  { "NEGEN",       { 96, 97, 98, 99, 100 } },
+  { "TIEN",        { 76, 77, 78, 79 } },
+  { "ELF",         { 86, 85, 84 } },
+  { "TWAALF",      { 92, 91, 90, 89, 88, 87 } }
 };
 
 const size_t WORDS_NL_20x20_V1_COUNT = sizeof(WORDS_NL_20x20_V1) / sizeof(WORDS_NL_20x20_V1[0]);
@@ -46,6 +46,6 @@ const size_t EXTRA_MINUTES_NL_20x20_V1_COUNT = sizeof(EXTRA_MINUTES_NL_20x20_V1)
 
 // LEDs to blink when no time is available (e.g. NTP sync failed)
 const uint16_t NO_TIME_INDICATOR_LEDS_NL_20x20_V1[] = {
-  102
+  101
 };
 const size_t NO_TIME_INDICATOR_LEDS_NL_20x20_V1_COUNT = sizeof(NO_TIME_INDICATOR_LEDS_NL_20x20_V1) / sizeof(NO_TIME_INDICATOR_LEDS_NL_20x20_V1[0]);
