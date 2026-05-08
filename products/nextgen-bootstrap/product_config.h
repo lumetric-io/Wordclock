39b644fec15d8542a90007a6984dcc8105aad116
Ron Kuijpers <ronkuijpers@gmail.com>
2026-05-08 16:05:10 +0200

    Bump nextgen-bootstrap version to nextgen-bootstrap-0.1.2

--- products/nextgen-bootstrap/product_config.h
+++ products/nextgen-bootstrap/product_config.h
@@ -6,8 +6,8 @@
 // to MQTT; most of that code is excluded via build_src_filter.
 
 #define PRODUCT_ID "nextgen-bootstrap"
-#define FIRMWARE_VERSION "nextgen-bootstrap-0.1.1"
-#define UI_VERSION "ui-nextgen-bootstrap-0.1.1"
+#define FIRMWARE_VERSION "nextgen-bootstrap-0.1.2"
+#define UI_VERSION "ui-nextgen-bootstrap-0.1.2"
 
 // Marker used by gated code in shared sources (ota_updater.cpp,
 // system_utils.cpp) to compile out per-device dependencies that
