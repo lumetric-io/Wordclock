596ea8c2874e689f732e87601597e1dce0eb73cb
Ron Kuijpers <ronkuijpers@gmail.com>
2026-05-26 21:35:18 +0200

    Bump nextgen-logo-55x50 version to nextgen-logo-55x50-25.5.26

--- products/nextgen-logo-55x50/product_config.h
+++ products/nextgen-logo-55x50/product_config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PRODUCT_ID "nextgen-logo-55x50"
-#define FIRMWARE_VERSION "nextgen-logo-55x50-25.5.10-rc.12"
-#define UI_VERSION "ui-nextgen-logo-55x50-25.5.10-rc.12"
+#define FIRMWARE_VERSION "nextgen-logo-55x50-25.5.26"
+#define UI_VERSION "ui-nextgen-logo-55x50-25.5.26"
 #define PRODUCT_VARIANT_LOGO 1
 #define DATA_PIN 4
 #define LOGO_DATA_PIN 18
