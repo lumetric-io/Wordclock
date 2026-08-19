637c3d8cfb591c84116db3904de39f9c476fc36f
Ron Kuijpers <ronkuijpers@gmail.com>
2026-08-19 22:50:57 +0200

    Bump nextgen-mini version to nextgen-mini-26.08.19

--- products/nextgen-mini/product_config.h
+++ products/nextgen-mini/product_config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PRODUCT_ID "nextgen-mini"
-#define FIRMWARE_VERSION "nextgen-mini-26.06.08"
-#define UI_VERSION "ui-nextgen-mini-26.06.08"
+#define FIRMWARE_VERSION "nextgen-mini-26.08.19"
+#define UI_VERSION "ui-nextgen-mini-26.08.19"
 #define PRODUCT_VARIANT_MINI 1
 #define DATA_PIN 4
 #define SUPPORT_MINUTE_LEDS 0
