db1662d352f2fa9a999a4986187a13061eb27524
Ron Kuijpers <ronkuijpers@gmail.com>
2026-08-20 14:46:45 +0200

    Bump nextgen-mini* versions to 26.08.20-dev.6

--- products/nextgen-30x30/product_config.h
+++ products/nextgen-30x30/product_config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PRODUCT_ID "nextgen-30x30"
-#define FIRMWARE_VERSION "nextgen-30x30-26.08.20-dev.5"
-#define UI_VERSION "ui-nextgen-30x30-26.08.20-dev.5"
+#define FIRMWARE_VERSION "nextgen-30x30-26.08.20-dev.6"
+#define UI_VERSION "ui-nextgen-30x30-26.08.20-dev.6"
 #define PRODUCT_VARIANT_NEXTGEN 1
 #define DATA_PIN 4
 #define SUPPORT_OTA_V2 1
--- products/nextgen-logo-55x50/product_config.h
+++ products/nextgen-logo-55x50/product_config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PRODUCT_ID "nextgen-logo-55x50"
-#define FIRMWARE_VERSION "nextgen-logo-55x50-26.08.20-dev.5"
-#define UI_VERSION "ui-nextgen-logo-55x50-26.08.20-dev.5"
+#define FIRMWARE_VERSION "nextgen-logo-55x50-26.08.20-dev.6"
+#define UI_VERSION "ui-nextgen-logo-55x50-26.08.20-dev.6"
 #define PRODUCT_VARIANT_LOGO 1
 #define DATA_PIN 4
 #define LOGO_DATA_PIN 18
