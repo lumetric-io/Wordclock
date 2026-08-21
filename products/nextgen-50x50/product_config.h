99604fa3b55ea22be294d0440c47302016d4b81e
Ron Kuijpers <ronkuijpers@gmail.com>
2026-08-21 11:05:21 +0200

    Bump nextgen-50x50 version to nextgen-50x50-26.08.21-rc.1

--- products/nextgen-50x50/product_config.h
+++ products/nextgen-50x50/product_config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PRODUCT_ID "nextgen-50x50"
-#define FIRMWARE_VERSION "nextgen-50x50-26.08.20-rc.3"
-#define UI_VERSION "ui-nextgen-50x50-26.08.20-rc.3"
+#define FIRMWARE_VERSION "nextgen-50x50-26.08.21-rc.1"
+#define UI_VERSION "ui-nextgen-50x50-26.08.21-rc.1"
 #define PRODUCT_VARIANT_NEXTGEN 1
 #define DATA_PIN 4
 #define SUPPORT_OTA_V2 1
